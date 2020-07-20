7a8e0ba8d817cd0f3c1d36e30c7abec2813f1be2
chenxing <chenxing@bookan.com.cn>
2020-07-20 09:55:23 +0800

    1

--- easy/2.5.cpp
+++ easy/2.5.cpp
@@ -1,8 +1,17 @@
 #include <stdio.h>
+#include <math.h>
+
+/*
+ * printf函数必须包含stdio.h，要使用数学函数必须包含math.h
+ */
 
 int main() {
     int hour = 11;
     int minute = 59;
     printf("%d hours and %d percent of an hour\n", hour, minute * 100 / 60);
     printf("%d and %f hours\n", hour, minute / 60.0);
+    printf("%c\n", 'a'+1);
+
+    double pi = 3.1416;
+    printf("sin(pi/2)=%f\nln1=%f\n", sin(pi/2), log(1.0));
 }
