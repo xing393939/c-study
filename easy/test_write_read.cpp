7cf6737d1b1a7fdb960b7281da7e72b4aededf15
chenxing <chenxing@bookan.com.cn>
2020-10-26 11:50:35 +0800

    1

---
+++ easy/test_write_read.cpp
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void) {
+    FILE *fp1 = fopen("int.txt", "wb");
+    int data1[4] = {1, 2, 3, 4};
+    fwrite(data1, sizeof(int), 4, fp1);
+    fclose(fp1);
+
+    FILE *fp2 = fopen("int.txt", "rb");
+    int data2[4];
+    fread(data2, sizeof(int), 4, fp2);
+    fclose(fp2);
+    for (int i : data2) {
+        printf("%d\n", i);
+    }
+
+    typedef struct {
+        int age;
+        char name[30];
+    } people;
+    FILE *fp3 = fopen("people.txt", "wb");
+    people per1[2];
+    per1[0].age = 18;
+    strcpy(per1[0].name, "wa");
+    per1[1].age = 20;
+    strcpy(per1[1].name, "li");
+    fwrite(per1, sizeof(people), 2, fp3);
+    fclose(fp3);
+
+    FILE *fp4 = fopen("people.txt", "rb");
+    people per2[20];
+    fread(per2, sizeof(people), 2, fp4);
+    fclose(fp4);
+    for (int i = 0; i < 2; i++) {
+        printf("%d %s\n", per2[i].age, per2[i].name);
+    }
+
+    return 0;
+}
