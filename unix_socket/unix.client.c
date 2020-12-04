12f2f3106423d8f19605e6a396d1f2516eeb0572
xing393939 <xing393939@users.noreply.github.com>
2020-12-04 15:34:31 +0800

    unix domain socket

---
+++ unix_socket/unix.client.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include "myfunc/wrap.h"
+
+#define MAXLINE 80
+#define SERV_PORT 8000
+int main(int argc, char *argv[])
+{
+
+    return 1;
+}
